c676ef28833ddf6f536803bb4e931a8f9249a777
Lucas E. <espiet.l@valbion.com>
2024-09-04 18:44:40 +0200

    📜 Ajout des polices personnalisées

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Outfit', 'sans-serif'],
+			serif: ['Myra', 'ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+			mono: ['"JetBrains Mono"', 'monospace']
+		},
 		extend: {},
 	},
 	plugins: [],
