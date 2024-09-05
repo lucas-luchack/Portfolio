ac3e8d9770032447a04e2eacadf54d1abc22f4b6
Lucas E. <espiet.l@valbion.com>
2024-09-05 18:09:48 +0200

    🎊 Add Logo and first navigation bar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,27 @@ export default {
 			serif: ['Myra', 'ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
 			mono: ['"JetBrains Mono"', 'monospace']
 		},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			background: {
+				0: '#141414',
+				1: '#1D1D1D',
+				2: '#1D1D1D',
+			},
+			text: {
+				primary: '#FFFFFF',
+				secondary: '#B3B3B3',
+				colored: '#45495D'
+			},
+			stroke: '#232323',
+			success: '#78FFD1',
+			warning: '#F0FFA6',
+			danger: '#FFB3C2',
+			information: '#99B9FF',
+			colors: {
+			},
+		},
 		extend: {},
 	},
 	plugins: [],
