7199bd35a153ec4764f94dfa067de9ed8152e5fa
Lucas ESPIET <lespiet@iutbayonne.univ-pau.fr>
2024-09-10 11:35:25 +0200

    ➕ Ajout de la config et des packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://luchack.dev",
+  integrations: [tailwind(), sitemap()],
+  output: "hybrid",
+  adapter: vercel(),
 });
