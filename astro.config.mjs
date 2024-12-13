cf378f3a0f9baa94bfebd324820160346ef7d60d
Lucas E. <espiet.l@valbion.com>
2024-12-13 16:21:41 +0100

    :pencil2: Mise à jour de la config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://luchack.dev",
   integrations: [tailwind(), sitemap()],
-  output: "hybrid",
+  output: "server",
   adapter: vercel(),
 });
